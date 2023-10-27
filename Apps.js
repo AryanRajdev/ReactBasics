1f1c737277ddbec4ea7736577bb96c5b7418898f
AryanRajdev <aryanraj24032002@gmail.com>
2023-10-27 21:08:05 +0530

    Episode 3

--- Apps.js
+++ Apps.js
@@ -1,18 +1,31 @@
-// {/* <div id = "parent">
-//     <div id = "child">
-//         <h1>I am h1 tag</h1>
-//     </div>
-// </div> */}
-
 import React from "react";
-import  ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
 
+// const parent = React.createElement("div",
+//  {id:"parent"},
+//  React.createElement("div",{id:"child"},
+//  React.createElement("h1",{},"i am h1 tag")));
 
-const parent = React.createElement("div",
- {id:"parent"}, 
- React.createElement("div",{id:"child"},
- React.createElement("h1",{},"i am h1 tag")));
+// React Element
+const jsxheading = (
+<h1 id="jsxhed">Heading using JSX Element</h1>
+);
+
+// React Component
+const Title =()=> (
+<h1 id="jsxhed">Heading using JSX Component</h1>
+);
+    
+const Heading = () =>(
+    <div>
+        {/* <Title/> */}
+        {Title()}
+        {jsxheading}
+        <h1 id = "heading">Functional React Component </h1>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+// root.render(jsxheading);
+root.render(<Heading/>);
